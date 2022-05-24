84bbec0c5ac6d16cf72deed70b688b8f86788f31
AlessanderSalgado <99378398+AlessanderSalgado@users.noreply.github.com>
2022-05-24 18:17:56 -0300

    Alterações na apiCarrefour 3

--- src/api/apiCarrefour.js
+++ src/api/apiCarrefour.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 const apiCep = axios.create({
   baseURL: 'https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=28921212',
-  mode: 'cors',
-  headers : {"X-VTEX-API-AppKey": "vtexappkey-carrefourbr-PZLYPS","X-VTEX-API-AppToken": "UTARJONVBFAYJEOZGNAUZIRIFLMBNUVOOLWNTLIORNRCMGSIVEQFCTNNYOXYGHPUYEVLDHUVPKUHHBDOZPBKMOKGRITGDBXVQBDESIDQJWDANEZSIQVGSCZVEVPLCJVC"}
-
+  headers: {"Access-Control-Allow-Origin": "*"}
 
 });
 
