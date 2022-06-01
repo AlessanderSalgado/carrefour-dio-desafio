bebb0c9f97681c641bc6c8011bac60d0dd63e8ac
AlessanderSalgado <99378398+AlessanderSalgado@users.noreply.github.com>
2022-06-01 17:14:15 -0300

    Organização do codigo 4

--- src/component/api/conect.js
+++ src/component/api/conect.js
@@ -3,4 +3,5 @@ const Conect = axios.create({
   //baseURL: 'https://mercado.carrefour.com.br/api/checkout/pub/regions?country=BRA&postalCode=28921212',
   baseURL:'https://00411bbc-6601-43da-85b8-048b253b7f03.mock.pstmn.io',
 });
+
 export default Conect;
--- src/component/logo/logo.js
+++ src/component/logo/logo.js
@@ -8,4 +8,5 @@ function Logo() {
     </span>
   );
 }
+
 export default Logo;
